db6df566615a106602baa988a1eead52bc6ad5b2
Ting-Yu. Lyu <b790113g@gmail.com>
2014-11-03 14:38:51 +0800

    fix plugins bug

--- js/tinymce/plugins/evimages/editor_plugin.js
+++ js/tinymce/plugins/evimages/editor_plugin.js
@@ -1,11 +1,11 @@
 (function() {
-	// Load plugin specific language pack
+
 	tinymce.PluginManager.requireLangPack('evimages');
 
 	tinymce.create('tinymce.plugins.evimagesPlugin', {
 		
 		init : function(ed, url) {
-			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
+
 			ed.addCommand('evimages', function() {
 				var unixtime_ms = new Date().getTime();
 				ed.windowManager.open({
@@ -14,7 +14,7 @@
 					height : 155 + parseInt(ed.getLang('evimages.delta_height', 0)),
 					inline : 1
 				}, {
-					plugin_url : url // Plugin absolute URL
+				   custom_param : 1
 				});
 			});
 
@@ -30,23 +30,12 @@
 				cm.setActive('evimages', n.nodeName == 'IMG');
 			});
 		},
-
-		/**
-		 * Creates control instances based in the incomming name. This method is normally not
-		 * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
-		 * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
-		 * method can be used to create those.
-		 *
-		 * @param {String} n Name of the control to create.
-		 * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
-		 * @return {tinymce.ui.Control} New control instance or null if no control was created.
-		 */
 		createControl : function(n, cm) {
 			return null;
 		},
 		getInfo : function() {
 			return {
-				author : 'Viktor Kuzhelnyi'
+				author : 'EvanLyu'
 			};
 		}
 	});
