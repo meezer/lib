7487204c6f63a0d1e2cd81a471d3f0e424a57d35
Ting-Yu. Lyu <b790113g@gmail.com>
2014-11-05 16:34:33 +0800

    fix tinymce

--- js/tinymce/plugins/evimages/editor_plugin.js
+++ js/tinymce/plugins/evimages/editor_plugin.js
@@ -5,32 +5,29 @@
 	tinymce.create('tinymce.plugins.evimagesPlugin', {
 		
 		init : function(ed, url) {
-/*
+
 			ed.addCommand('evimages', function() {
 				var unixtime_ms = new Date().getTime();
 				ed.windowManager.open({
-					file : url + '/dialog.htm?z' + unixtime_ms,
-					width : 330 + parseInt(ed.getLang('evimages.delta_width', 0)),
-					height : 155 + parseInt(ed.getLang('evimages.delta_height', 0)),
+					file : '/showUploadDialog' + unixtime_ms,
+					width : 300 + parseInt(ed.getLang('evimages.delta_width', 0)),
+					height : 120 + parseInt(ed.getLang('evimages.delta_height', 0)),
 					inline : 1
 				}, {
-				   custom_param : 1
+					plugin_url : url
 				});
 			});
 
-			// Register example button
 			ed.addButton('evimages', {
 				title : 'evimages.desc',
 				cmd : 'evimages',
 				image : url + '/img/evimages-bw.gif'
 			});
-
-			// Add a node change handler, selects the button in the UI when a image is selected
+			
 			ed.onNodeChange.add(function(ed, cm, n) {
 				cm.setActive('evimages', n.nodeName == 'IMG');
 			});
 
-*/
 		},
 		createControl : function(n, cm) {
 			return null;
